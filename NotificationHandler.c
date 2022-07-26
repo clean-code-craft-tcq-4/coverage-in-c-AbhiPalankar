7c45a53bc223682447b75b68a86dfb2b67bd6fdc
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-07-26 21:43:38 +0530

    Add files via upload
    Added Notification handler file for separation

---
+++ NotificationHandler.c
@@ -0,0 +1,37 @@
+#include "typewise-alert.h"
+
+
+StatusType (*EmailMesssage[])(const char* recepient) = { sendNormalMessage, sendTooLowMessage , sendTooHighMessage };
+
+
+StatusType sendToEmail(BreachType breachType) 
+{
+  const char* recepient = "a.b@c.com";
+  StatusType EmailMesssageNotify = (*EmailMesssage[breachType])(recepient);
+	  return EmailMesssageNotify;
+
+}
+
+StatusType sendNormalMessage(const char* recepient)
+{
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is Normal\n");
+	  return SEND_TO_EMAIL_NORMAL;
+}
+
+StatusType sendTooLowMessage(const char* recepient)
+{
+	  printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too low\n");
+	  return SEND_TO_EMAIL_TOO_LOW;
+
+}
+
+StatusType sendTooHighMessage(const char* recepient)
+{
+      printf("To: %s\n", recepient);
+      printf("Hi, the temperature is too high\n");
+	  return SEND_TO_EMAIL_TOO_HIGH;
+}
+
+
